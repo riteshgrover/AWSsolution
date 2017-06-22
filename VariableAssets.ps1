3a4ca6427502056bf1e6bd779c7d725d815bb44e
riteshgrover <ritesh_grover@live.com>
2017-06-22 19:27:46 +0530

    Add files via upload

--- VariableAssets.ps1
+++ VariableAssets.ps1
@@ -1,3 +1,2 @@
-New-AzureRmAutomationVariable -ResourceGroupName "AutomationAccountRG1" 
-–AutomationAccountName "AutomationAccount1" –Name 'MyStringVariable' `
-–Encrypted $false –Value 'My String'
+write-output "This is Test"
+
